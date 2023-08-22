80ca2db5f051b0fbaf56fe63e2147dd714af52c0
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-08-22 10:23:42 +0900

    param_table
    param_table

--- src/components/ApiRequestTabsContainer/ParamsTab.jsx
+++ src/components/ApiRequestTabsContainer/ParamsTab.jsx
@@ -3,20 +3,39 @@ import '../../style/paramstab.scss';
 
 
 export default function ParamsTab() {
-    return (
-        <div className="params_container">
-          <div className='params_editor_title'>
-            <span>Query Params</span>
-          </div>
-          <div className='params_editor_table'>
-            <div className='params_editor_header_row'>
-                <div className='params_editor_top_header'>
-                <div className='params_editor_key_header_row '>
-                    key
+  return (
+    <div className="params_container">
+      <div className='params_editor_title'>
+        <span>Query Params</span>
+      </div>
+      <div className='params_editor_container'>
+        <div className='params_editor_header_row'>
+          <div className='params_editor_top_header'>
+            <div className='params_editor_key_header_row '>
+              <div className='params_form_header_row'></div>
+              <div className='params_header_row'>
+                <div className="param_row">Key</div>
+                <div className="param_row">Value</div>
+                <div className="param_row">Description</div>
+              </div>
+            </div>
+            <div className='params_editor_key_header_row ' style={{ borderTop: 'none' }}>
+              <div className='params_form_header_row'></div>
+              <div className='params_header_row'>
+                <div className="param_input_row">
+                  <input className="param_row" type="text" placeholder="Key" />
                 </div>
+                <div className="param_input_row">
+                  <input className="param_row" type="text" placeholder="Value" />
                 </div>
+                <div className="param_input_row">
+                  <input className="param_row" type="text" placeholder="Description" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    </div>
+  );
+}
