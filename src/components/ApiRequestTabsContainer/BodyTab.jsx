a70ca3b684db7ee692ea0fbded6addadffceefed
heejinkong <113762366+heejinkong@users.noreply.github.com>
2023-08-22 14:47:18 +0900

    bodytab
    bodytab

--- src/components/ApiRequestTabsContainer/BodyTab.jsx
+++ src/components/ApiRequestTabsContainer/BodyTab.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import '../../style/bodytab.scss';
+import { Radio } from '@mui/material';
 
 export default function BodyTab() {
-  return <div>Body</div>;
+  const [selectedValue, setSelectedValue] = React.useState('a');
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const controlProps = (item) => ({
+    checked: selectedValue === item,
+    onChange: handleChange,
+    value: item,
+    name: 'size-radio-button-demo',
+    inputProps: { 'aria-label': item },
+  });
+
+  
+
+  return (
+    <div className='body_container'>
+      <div className='body_request_type'>
+        <div className='body_request_form'>
+          <Radio {...controlProps('a')} size="10px" />
+          <span>form-data</span>
+        </div>
+        <div className="body_request_raw">
+          <Radio {...controlProps('b')} size="10px" />
+          <span>raw</span>
+        </div>
+     </div>
+     <div className='body_editor_container'>
+
+     </div>
+    </div>
+  );
 }
