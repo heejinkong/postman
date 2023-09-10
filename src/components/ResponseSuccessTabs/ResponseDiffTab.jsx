376ca1120825a449808b3c97f14f5df7406eadcc
heejinkong <gmlwls3379@naver.com>
2023-09-10 19:02:40 +0900

    값 가져와서 diff
    값 가져와서 diff

--- src/components/ResponseSuccessTabs/ResponseDiffTab.jsx
+++ src/components/ResponseSuccessTabs/ResponseDiffTab.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as Diff2Html from "diff2html";
 import '../../style/diff2html.css';
 
+import { useData } from '../../contexts/DataContext';
+
 export default function ResponseDiffTab() {
-  // 직접 diff 데이터 생성
-  const diff = `diff --git a/sample.js b/sample.js
-index 0000001..0ddf2ba
---- a/sample.js
-+++ b/sample.js
-@@ -1 +1 @@
--console.log("Hello World!")
-+console.log("Hello from Diff2Html!")`;
+  const { resultText, resultData } = useData();
+
+  const Diff = require("diff");
+
+  const diff = Diff.createTwoFilesPatch(
+    "예상값",
+    "응답값",
+    `${resultText}`,`${resultData}`
+  );
 
   let outputHtml = Diff2Html.html(diff, {
     inputFormat: "diff",
     showFiles: true,
     matching: "lines",
+    highlight: true,
     outputFormat: "side-by-side"
   });
 
   return (
     <div>
-      <h2>Code Difference</h2>
-      <div dangerouslySetInnerHTML={{ __html: outputHtml }}></div>
-    </div>
+    <h2>Code Difference</h2>
+    {/* diff 결과를 화면에 표시 */}
+    <div dangerouslySetInnerHTML={{ __html: outputHtml }}></div>
+  </div>
+
   );
 }
