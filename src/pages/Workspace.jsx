2d697ac256d0f0d7f4bfd14badd79d4810d2b558
heejinkong <gmlwls3379@naver.com>
2023-10-03 16:33:11 +0900

    workspace 수정찐
    workspace 수정찐

--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -5,71 +5,72 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 
 export default function Workspace(props) {
-  const { workspaceId } = useParams(); 
-  const [id, setId] = useState(0); // 초기 id 값을 0으로 설정
-  const [workspaceName, setWorkspaceName] = useState('');
+  const { workspaceId } = useParams();
+  const [workspaceData, setWorkspaceData] = useState({
+    id: 0,
+    name: '',
+    description: '',
+  });
   const [isEditingDescription, setEditingDescription] = useState(false);
-  const [descriptionText, setDescriptionText] = useState('');
   const location = useLocation();
 
-  // 페이지 로드 시 로컬 스토리지에서 데이터 읽어오기
-useEffect(() => {
-  if (workspaceId !== 'new') {
-    const workspaceData = localStorage.getItem(`workspace-${workspaceId}`);
-    const workspace = workspaceData ? JSON.parse(workspaceData) : null;
+  useEffect(() => {
+    if (workspaceId !== 'new') {
+      const workspaceData = localStorage.getItem(`workspace-${workspaceId}`);
+      const workspace = workspaceData ? JSON.parse(workspaceData) : null;
 
-    if (workspace) {
-      setId(workspace.id);
-      setWorkspaceName(workspace.name);
-      setDescriptionText(workspace.description);
+      if (workspace) {
+        setWorkspaceData(workspace);
+      }
+    } else {
+      let nextId = 1;
+      while (localStorage.getItem(`workspace-${nextId}`)) {
+        nextId++;
+      }
+      setWorkspaceData({ id: nextId, name: '', description: '' });
     }
-  } else {
-    // "new" 경로로 들어온 경우, 새로운 id를 동적으로 할당합니다.
-    let nextId = 1;
-    while (localStorage.getItem(`workspace-${nextId}`)) {
-      nextId++;
-    }
-    setId(nextId);
-  }
-}, [workspaceId]);
-
+  }, [workspaceId]);
 
   const handleNameChange = (e) => {
     const newWorkspaceName = e.target.value;
-    setWorkspaceName(newWorkspaceName);
-
-    const workspaceData = localStorage.getItem(`workspace-${id}`);
-    if (workspaceData) {
-      const workspace = JSON.parse(workspaceData);
-      workspace.name = newWorkspaceName;
-      localStorage.setItem(`workspace-${id}`, JSON.stringify(workspace));
-    }
+    setWorkspaceData((prevData) => ({
+      ...prevData,
+      name: newWorkspaceName,
+    }));
+    saveWorkspaceDataToLocalStorage({
+      ...workspaceData,
+      name: newWorkspaceName,
+    });
   };
 
   const handleDescriptionChange = (e) => {
     const newDescriptionText = e.target.value;
-    setDescriptionText(newDescriptionText);
-
-    const workspaceData = localStorage.getItem(`workspace-${id}`);
-    if (workspaceData) {
-      const workspace = JSON.parse(workspaceData);
-      workspace.description = newDescriptionText;
-      localStorage.setItem(`workspace-${id}`, JSON.stringify(workspace));
-    }
+    setWorkspaceData((prevData) => ({
+      ...prevData,
+      description: newDescriptionText,
+    }));
+    saveWorkspaceDataToLocalStorage({
+      ...workspaceData,
+      description: newDescriptionText,
+    });
   };
 
   const handleDescriptionBlur = () => {
     setEditingDescription(false);
+    saveWorkspaceDataToLocalStorage(workspaceData);
   };
-
+ 
   const handleNotesClick = () => {
     setEditingDescription(true);
   };
 
+  const saveWorkspaceDataToLocalStorage = (data) => {
+    localStorage.setItem(`workspace-${data.id}`, JSON.stringify(data));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-  
-    // 로컬 스토리지에서 현재 가장 큰 ID를 찾기
+
     let maxId = 0;
     for (let i = 1; i <= localStorage.length; i++) {
       const key = localStorage.key(i);
@@ -80,25 +81,16 @@ useEffect(() => {
         }
       }
     }
-  
-    // 새로운 ID 계산
+
     const newId = maxId + 1;
-  
-    const workspace = { id: newId, name: workspaceName, description: descriptionText };
-  
-    localStorage.setItem(`workspace-${newId}`, JSON.stringify(workspace));
-  
-    // `id` 상태를 업데이트합니다.
-    setId(newId);
-  
-    // 워크스페이스 정보를 업데이트한 후 현재 워크스페이스 페이지로 리다이렉트
+    const workspaceDataToSave = { ...workspaceData, id: newId };
+    saveWorkspaceDataToLocalStorage(workspaceDataToSave);
+
+    setWorkspaceData(workspaceDataToSave);
     window.location.href = `/workspace/${newId}`;
   };
-  
-  
 
-
-  const isWorkspaceRoute = location.pathname === `/workspace/${id}`;
+  const isWorkspaceRoute = location.pathname === `/workspace/${workspaceData.id}`;
 
   return (
     <div className="workspace_container">
@@ -107,15 +99,15 @@ useEffect(() => {
         <input
           type="text"
           name="workspacetitle"
-          value={workspaceName}
+          value={workspaceData.name}
           onChange={handleNameChange}
           placeholder="My Workspace"
         />
 
-        {id < 1 ? (
-          <button onClick={handleSubmit}>save</button>
+        {workspaceData.id < 1 ? (
+          <button onClick={handleSubmit}>Save</button>
         ) : (
-          <Link to={`/workspace/${id}?isForEdit=true`}></Link>
+          <Link to={`/workspace/${workspaceData.id}?isForEdit=true`}></Link>
         )}
       </div>
       <div className="workspace_description">
@@ -127,14 +119,14 @@ useEffect(() => {
             {isEditingDescription ? (
               <textarea
                 name="description"
-                value={descriptionText}
+                value={workspaceData.description}
                 onChange={handleDescriptionChange}
                 onBlur={handleDescriptionBlur}
                 autoFocus
               />
             ) : (
               <span>
-                {descriptionText ||
+                {workspaceData.description ||
                   'Add information that you want quick access to. It can include links to important resources or notes of what you want to remember.'}
               </span>
             )}
