7424430f2a3e917a0ddd15d511233069bcdbf148
heejinkong <gmlwls3379@naver.com>
2023-10-09 13:53:18 +0900

    해당 workspace id와 collection 연결

--- src/pages/CollectionEditor.jsx
+++ src/pages/CollectionEditor.jsx
@@ -8,135 +8,172 @@ import { Link ,useParams, useLocation } from 'react-router-dom';
 
 
 export default function CollectionEditor(props) {
-
-  const dispatch =  useDispatch();
   const { workspaceId, collectionId } = useParams();
-  const [collectionName, setCollectionName] = useState('');
+  const [collectionData, setCollectionData] = useState({
+    id: 0,
+    collectionname: "",
+    collectiontext:"",
+    date: Date.now(),
+    collections: [],
+    workspaceId: 0,
+  });
   const [isEditingDescription, setEditingDescription] = useState(false);
-  const [collectionText, setCollectionText] = useState('');
-  const [isForUpdate, setIsForUpdate] = useState(false);
   const location = useLocation();
 
-  const { id ,collectionname, collectiontext } = useSelector(
-    (state) => ({
-    id: state.collectionReducers.id,
-    collectionname: state.collectionReducers.collectionname,
-    collectiontext: state.collectionReducers.collectiontext,
-  }),
-  shallowEqual
-  );
-console.log(collectionId);
+  useEffect(() => {
+    if (collectionId !== ':collectionId') {
+      const collectionData = localStorage.getItem(`collection-${collectionId}`);
+      const collection = collectionData ? JSON.parse(collectionData) : null;
+  
+      if (collection) {
+        setCollectionData(collection);
+      }
+    } else {
+      // 기존에 사용된 ID 중 가장 큰 ID를 찾아서 그 다음 ID를 설정
+      let maxId = 0;
+      for (let i = 1; i <= localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key && key.startsWith('collection-')) {
+          const id = parseInt(key.replace('collection-', ''), 10);
+          if (!isNaN(id) && id > maxId) {
+            maxId = id;
+          }
+        }
+      }
+  
+      const nextId = maxId + 1;
+  
+      setCollectionData({ id: nextId, collectionname: '', collectiontext: '' });
+    }
+  }, [collectionId]);
+ 
   const handleNameChange = (e) => {
-    setCollectionName(e.target.value);
+    const newCollectionName = e.target.value;
+    setCollectionData((prevData) => ({
+      ...prevData,
+      collectionname: newCollectionName,
+    }));
+    saveCollectionDataToLocalStorage({
+      ...collectionData,
+      collectionname: newCollectionName,
+    });
   };
 
   const handleDescriptionChange = (e) => {
-    setCollectionText(e.target.value);
+    const newDescriptionText = e.target.value;
+    setCollectionData((prevData) => ({
+      ...prevData,
+      collectiontext: newDescriptionText,
+    }));
+    saveCollectionDataToLocalStorage({
+      ...collectionData,
+      collectiontext: newDescriptionText,
+    });
   };
 
   const handleDescriptionBlur = () => {
     setEditingDescription(false);
+    saveCollectionDataToLocalStorage(collectionData);
   };
+
   const handleNotesClick = () => {
     setEditingDescription(true);
   };
 
+  const saveCollectionDataToLocalStorage = (data) => {
+    localStorage.setItem(`collection-${data.id}`, JSON.stringify(data));
+  };
+
   const handleSave = (e) => {
     e.preventDefault();
+  
+    // 기존에 사용된 ID 중 가장 큰 ID를 찾아서 그 다음 ID를 설정
+    let maxId = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key && key.startsWith('collection-')) {
+        const id = parseInt(key.replace('collection-', ''), 10);
+        if (!isNaN(id) && id > maxId) {
+          maxId = id;
+        }
+      }
+    }
+  
+    const newId = maxId + 1;
+  
     const collection = {
-      id: 0,
-      collectionname: collectionName,
-      collectiontext: collectionText,
+      id: newId,
+      collectionname: collectionData.collectionname,
+      collectiontext: collectionData.collectiontext,
       date: Date.now(),
-      collections:[],
-      workspaceId: id,
+      collections: [],
+      workspaceId: workspaceId,
     };
+  
+    localStorage.setItem(`collection-${collection.id}`, JSON.stringify(collection));
 
-   
-      dispatch(collectionActions.registerCollection(collection));
-
+    const newUrl = `/workspace/${workspaceId}/collection/${collection.id}`;
+  window.location.href = newUrl;
   };
+  
 
   
   const collections = useSelector((state) => state.collectionReducers.collections);
-console.log(collections);
 
 
-
-  useEffect(() => {
-    dispatch(collectionActions.getCollection(workspaceId));
-  }, [dispatch, workspaceId])
-
-
-useEffect(() => {
-  dispatch(collectionActions.getCollection({ workspaceId, collectionId }));
-}, [dispatch,(workspaceId, collectionId)])
-
-useEffect(() => {
-  const searchParams = new URLSearchParams(location.search);
-  if (searchParams.get("isForEdit") === "true") {
-    setIsForUpdate(true);
-    dispatch(collectionActions.fetchCollection(collectionId));
-  }
-}, [location.search, workspaceId]);
-
-
-
-useEffect(() => {
-  if (collectionId > 0) {
-    setCollectionName(collectionname);
-    setCollectionText(collectiontext);
-  }
-}, [collectionname, collectiontext]);
-
-  return (
-    <div>
-      <div className='collection_editor_container'>
-        <div className='collection_path_container'>
-            <div className='collection_path_title'>
-                <input
-                type='text'
-                value={collectionName}
-                onChange={handleNameChange}
-                placeholder='New Collectoin'
+return (
+  <div>
+    <div className="collection_editor_container">
+      <div className="collection_path_container">
+        <div className="collection_path_title">
+          <input
+            type="text"
+            value={collectionData.collectionname}
+            onChange={handleNameChange}
+            placeholder="New Collection"
           />
-             </div>
-             <div className='collection_save_container'>
-                <div className='collection_save_btn'>
-                {collectionId > 0 ? (
-          <Link to={`/workspace/${workspaceId}/collection/${collectionId}?isForEdit=true`}></Link>
-        ) : (
-          <Button variant='outlined' onClick={handleSave} startIcon={<SaveIcon />} size='big'>
-          Save
-        </Button> 
-        )}
-                </div>
-
-             </div>
-         </div>
-        <div className='collection_description'>
-          <div className='collection_title'>
-            <textarea
-              value={collectionName}
-              onChange={handleNameChange}
-              style={{ fontWeight: 'bold' }}
-              placeholder='New Collectoin'
-            ></textarea>
-          </div>
-          <div className='collection_description_notes' onClick={handleNotesClick}>
-            {isEditingDescription ? (
-              <textarea
-                value={collectionText}
-                onChange={handleDescriptionChange}
-                onBlur={handleDescriptionBlur}
-                autoFocus
-              />
+        </div>
+        <div className="collection_save_container">
+          <div className="collection_save_btn">
+            {collectionData.id > 0 ? (
+              <Link
+                to={`/workspace/${workspaceId}/collection/${collectionData.id}?isForEdit=true`}
+              ></Link>
             ) : (
-              <span>{collectionText || 'Make things easier for you teammates with a complete collection description.'}</span>
+              <button onClick={handleSave}>
+                Save
+                <SaveIcon />
+              </button>
             )}
           </div>
         </div>
       </div>
+      <div className="collection_description">
+        <div className="collection_title">
+          <textarea
+            value={collectionData.collectionname}
+            onChange={handleNameChange}
+            style={{ fontWeight: 'bold' }}
+            placeholder="New Collection"
+          ></textarea>
+        </div>
+        <div className="collection_description_notes" onClick={handleNotesClick}>
+          {isEditingDescription ? (
+            <textarea
+              value={collectionData.collectiontext}
+              onChange={handleDescriptionChange}
+              onBlur={handleDescriptionBlur}
+              autoFocus
+            />
+          ) : (
+            <span>
+              {collectionData.collectiontext ||
+                'Make things easier for your teammates with a complete collection description.'}
+            </span>
+          )}
+        </div>
+      </div>
     </div>
-  );
+  </div>
+);
 }
