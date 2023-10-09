6023d5cbe8098d9379ea9863dd5f6077ab0026d2
heejinkong <gmlwls3379@naver.com>
2023-10-09 16:10:18 +0900

    workspace 리스트페이지 리듀서 삭제

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,51 +1,64 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 import '../style/listpage.scss';
-import { listActions } from '../slice/listSlice';
 import WorkspaceList from './WorkspaceList';
 
 const ListPage = () => {
-    const dispatch = useDispatch();
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
 
-    useEffect(() => {
-        dispatch(listActions.getList());
-    }, [dispatch]);
+  useEffect(() => {
+    const getListFromLocalStorage = () => {
+      const workspaceData = Object.keys(localStorage)
+        .filter((key) => key.startsWith("workspace-"))
+        .map((key) => JSON.parse(localStorage.getItem(key)));
 
-    const { list, isLoding, isSuccess } = useSelector((state) => ({
-        list: state.listReducers.list,
-        isLoding: state.listReducers.isLoding,
-        isSuccess: state.listReducers.isSuccess,
-    }));
+      if (workspaceData.length > 0) {
+        setList(workspaceData);
+        setIsSuccess(true);
+      }
 
-    return (
-        <div>
+      setIsLoading(false);
+    };
+
+    getListFromLocalStorage();
+  }, []);
+
+  const handleDeleteClick = (workspaceId) => {
+    if (!window.confirm("해당 Workspace를 삭제하시겠습니까 ?")) return false;
+
+    // 로컬 스토리지에서 Workspace 데이터 삭제
+    localStorage.removeItem(`workspace-${workspaceId}`);
+
+    // 작업 목록 업데이트
+    setList((prevList) => prevList.filter((workspace) => workspace.id !== workspaceId));
+  };
+
+  return (
+    <div>
       <div className='home_editor_container'>
         <div className='home_path_container'>
-            <div className='home_path_title'>
-               
-             </div>
-
-         </div>
+          <div className='home_path_title'></div>
+        </div>
         <div className='home_description'>
           <div className='home_title'>
-          {isSuccess && list.length > 0 ? (
-                   <p>Click a Workspace</p>
-                ) : (
-                    <p>Workspace does not exist </p>
-                )}
+            {isSuccess && list.length > 0 ? (
+              <p>Click a Workspace</p>
+            ) : (
+              <p>Workspace does not exist</p>
+            )}
           </div>
-          <div className='home_description_notes' >
-          {isSuccess && list.length > 0 ? (
-                    <WorkspaceList list={list}/>
-                ) : (
-                    <p> Create a Workspace using the 'New' button on the left </p>
-                )}
+          <div className='home_description_notes'>
+            {isSuccess && list.length > 0 ? (
+              <WorkspaceList list={list} handleDeleteClick={handleDeleteClick} />
+            ) : (
+              <p>Create a Workspace using the 'New' button on the left</p>
+            )}
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default ListPage;
--- src/pages/WorkspaceList.jsx
+++ src/pages/WorkspaceList.jsx
@@ -1,52 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux"; 
-import { workspaceActions } from "../slice/workspaceSlice";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const WorkspaceList = (props) => {
-  const dispatch = useDispatch(); 
-
   const handleDeleteClick = (workspaceId) => {
     if (!window.confirm("해당 Workspace를 삭제하시겠습니까 ?")) return false;
-    dispatch(workspaceActions.deleteWorkspace(workspaceId)); 
+
+    // 로컬 스토리지에서 Workspace 데이터 삭제
+    localStorage.removeItem(`workspace-${workspaceId}`);
+
+    // 작업 목록 업데이트
+    props.setList((prevList) => prevList.filter((workspace) => workspace.id !== workspaceId));
   };
 
   return (
     <div style={{ padding: "0 12px" }}>
-    <table style={{ width: "100%" }}>
-      <colgroup>
-        <col width="20%" />
-        <col width="70%" />
-        <col width="10%" />
-      </colgroup>
-      <thead>
-        <tr>
-          <th style={{ paddingRight: "32px" }}>id</th>
-          <th>Title</th>
-          <th ></th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.list.map((workspace) => (
-          <tr key={workspace.id}>
-            <td>{workspace.id}</td>
-            <td>
-              <Link to={`/workspace/${workspace.id}`} style={{ textDecoration: 'none', color: 'black' }}>{workspace.name}</Link>
-            </td>
-            <td style={{ paddingLeft: "100px" }}>
-              <IconButton onClick={() => handleDeleteClick(workspace.id)} aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            </td>
+      <table style={{ width: "100%" }}>
+        <colgroup>
+          <col width="20%" />
+          <col width="70%" />
+          <col width="10%" />
+        </colgroup>
+        <thead>
+          <tr>
+            <th style={{ paddingRight: "32px" }}>id</th>
+            <th>Title</th>
+            <th ></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-  
+        </thead>
+        <tbody>
+          {props.list.map((workspace) => (
+            <tr key={workspace.id}>
+              <td>{workspace.id}</td>
+              <td>
+                <Link to={`/workspace/${workspace.id}`} style={{ textDecoration: 'none', color: 'black' }}>{workspace.name}</Link>
+              </td>
+              <td style={{ paddingLeft: "100px" }}>
+                <IconButton onClick={() => handleDeleteClick(workspace.id)} aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
 export default WorkspaceList;
+
