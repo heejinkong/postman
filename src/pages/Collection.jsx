c7e78003769e733ba7ba1c069bbea231ddc54fec
heejinkong <gmlwls3379@naver.com>
2023-10-17 08:54:12 +0900

    sidebar 리스트 수정중

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,7 +11,7 @@ import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Collapse from '@mui/material/Collapse';
 import { List, ListItemIcon, ListItemText } from '@mui/material';
-import { Star } from '@mui/icons-material';
+import StarBorder from '@mui/icons-material/StarBorder';
 
 const options = [
   'Move',
@@ -28,10 +28,13 @@ export default function Collection(props) {
   const open = Boolean(anchorEl);
   const [collections, setCollections] = useState([]);
   const navigate = useNavigate();
-  const [openList, setOpenList] = useState(true);
+  const [openList, setOpenList] = useState(false);
+
+  const handleListClick = () => {
+    setOpenList(!openList);
+  };
 
   useEffect(() => {
-    // 로컬 스토리지에서 컬렉션 데이터를 불러옵니다.
     const loadCollectionsFromLocalStorage = () => {
       const collections = [];
       for (let i = 0; i < localStorage.length; i++) {
@@ -59,21 +62,20 @@ export default function Collection(props) {
     if (option === 'Delete') {
       handleDeleteClick(collectionId);
     } else if (option === 'Move') {
-      // 이동 로직 추가
+      // Add logic for moving the collection
     } else if (option === 'Run collection') {
-      // 컬렉션 실행 로직 추가
+      // Add logic for running the collection
     } else if (option === 'Add request') {
       navigate(`/workspace/${workspaceId}/collection/${collectionId}/request`);
     }
   };
 
   const handleDeleteClick = (collectionId) => {
-    if (!window.confirm('해당 collection를 삭제하시겠습니까 ?')) return false;
+    if (!window.confirm('Are you sure you want to delete this collection?'))
+      return false;
 
-    // 로컬 스토리지에서 컬렉션 데이터 삭제
     localStorage.removeItem(`collection-${workspaceId}-${collectionId}`);
 
-    // 컬렉션 목록 업데이트
     setCollections((prevCollections) =>
       prevCollections.filter((collection) => collection.id !== collectionId)
     );
@@ -81,85 +83,55 @@ export default function Collection(props) {
     navigate(`/workspace/${workspaceId}/collection/:collectionId`);
   };
 
-  const handleListClick = () => {
-    setOpenList(!open);
-  };
-
   return (
     <div>
       <div className="collection_container">
-        {collections.map((collection) => (
+        <ListItemButton onClick={handleListClick}>
+          <ListItemText primary="Inbox" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary="Starred" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+
+        {/* {collections.map((collection) => (
           <div key={collection.id} className="collection_list">
             <ListItemButton
               onClick={handleListClick}
               className="collection_list_btn"
             >
-              {/* <ChevronRightIcon /> */}
-              {open ? <ExpandLess /> : <ExpandMore />}
+              {openList ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
 
-            <button className="collection_list_name">
+            <div className="collection_list_name">
               <Link
                 to={`/workspace/${workspaceId}/collection/${collection.id}`}
                 style={{ textDecoration: 'none', color: 'black' }}
               >
                 {collection.collectionname}
               </Link>
-            </button>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemIcon>
-                    <Star />
-                  </ListItemIcon>
-                  <ListItemText primary="Starred" />
-                </ListItemButton>
-              </List>
-            </Collapse>
-
-            <div
-              className="collection_list_options"
-              style={{ opacity: open ? 1 : 0 }}
-            >
-              <IconButton
-                disableRipple
-                aria-label="more"
-                id="long-button"
-                aria-controls={open ? 'long-menu' : undefined}
-                aria-expanded={open ? 'true' : undefined}
-                aria-haspopup="true"
-                onClick={handleClick}
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                id="long-menu"
-                MenuListProps={{
-                  'aria-labelledby': 'long-button',
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                PaperProps={{
-                  style: {
-                    maxHeight: ITEM_HEIGHT * 4.5,
-                    width: '20ch',
-                  },
-                }}
-              >
-                {options.map((option) => (
-                  <MenuItem
-                    key={option}
-                    selected={option === 'Delete'}
-                    onClick={() => handleOptionClick(option, collection.id)}
-                  >
-                    {option}
-                  </MenuItem>
-                ))}
-              </Menu>
             </div>
-          </div>
-        ))}
+            <div>
+              <Collapse in={openList} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            </div> */}
+        {/* </div>
+        ))} */}
       </div>
     </div>
   );
